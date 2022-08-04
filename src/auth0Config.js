1e7a1af068099e58e70230421941bd6f679c6419
igor <igor1_souza@hotmail.com>
2022-08-04 10:33:16 -0400

    working

--- src/auth0Config.js
+++ src/auth0Config.js
@@ -1,9 +1,9 @@
 import { createAuth0 } from '@auth0/auth0-vue';
 
 const auth0 = createAuth0({
-    domain: DOMAIN_AUTH,
-    client_id: CLIENT_ID_AUTH,
-    redirect_uri: REDIRECT_AUTH
+    domain: 'dev-d8k6skw4.us.auth0.com',
+    client_id: '0gS5gRHJvzz1bf1Cog8BtIAqBFQRtiQQ',
+    redirect_uri: 'https://pwa-da1.pages.dev/#/'
   })
 
 export default auth0
