2a88dfa0b3c87e9a5e7041e4241b8f3424351f87
igor <igor1_souza@hotmail.com>
2022-08-04 10:18:31 -0400

    change variavel authconfig

--- src/auth0Config.js
+++ src/auth0Config.js
@@ -1,9 +1,9 @@
 import { createAuth0 } from '@auth0/auth0-vue';
 
 const auth0 = createAuth0({
-    domain: "dev-d8k6skw4.us.auth0.com",
-    client_id: "0gS5gRHJvzz1bf1Cog8BtIAqBFQRtiQQ",
-    redirect_uri: "https://pwa-da1.pages.dev/#/"
+    domain: DOMAIN_AUTH,
+    client_id: CLIENT_ID_AUTH,
+    redirect_uri: REDIRECT_AUTH
   })
 
 export default auth0
