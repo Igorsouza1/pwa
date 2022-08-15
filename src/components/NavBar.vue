ad364dd259405c66da51f387fc114abe1b5d3760
igor <igor1_souza@hotmail.com>
2022-08-15 08:17:05 -0400

    new background-color navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <nav class="navbar navbar-light bg-secondary">
+        <nav class="navbar navbar-light bg-dark bg-gradient">
             <a class="navbar-brand m-1" >
                 <img src="../assets/c.png" width="30" height="30"
                     class="d-inline-block align-top" alt="Icone letra C">
-                Cavalo Pantaneiro
+                <p class="text-white d-inline p">Cavalo Pantaneiro</p>
             </a>
         </nav>
     </div>
